5408a4f45db325ee94a3c15b47474e81e2ce218b
Dosso <massidedosso@gmail.com>
2024-06-05 12:45:38 +0200

    - creation of Module files with differents structs needed in app

---
+++ FireLearn/Module.swift
@@ -0,0 +1,34 @@
+//
+//  Module.swift
+//  FireLearn
+//
+//  Created by Apprenant 167 on 05/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Module: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+    let description: String
+}
+
+
+struct ModuleList {
+    
+    static let listOfModules = [
+        Module(imageName: "swift.png", title: "Swift", description: "Track your learning journey in Swift"),
+        Module(imageName: "swiftui.png", title: "Swift", description: "Track your learning journey in SwiftUI"),
+        Module(imageName: "xcode.png", title: "Xcode", description: "Track your learning journey in Xcode"),
+        Module(imageName: "vapor.png", title: "Vapor", description: "Track your learning journey in Vapor"),
+        Module(imageName: "github.png", title: "GitHub", description: "Track your learning journey in GitHub"),
+        Module(imageName: "swifdata.png", title: "Swift Data", description: "Track your learning journey in Swift Data"),
+        Module(imageName: "spritekit.png", title: "Sprite Kit", description: "Track your learning journey in Sprite Kit"),
+        Module(imageName: "arkit.png", title: "AR Kit", description: "Track your learning journey in AR Kit"),
+        Module(imageName: "poo.png", title: "Programmation orienté object", description: "Track your learning journey in POO")
+        
+    ]
+    
+}
