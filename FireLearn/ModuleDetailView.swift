4dee399891ae5f5d47b913909783459e2a7b60fe
Dosso <massidedosso@gmail.com>
2024-06-06 11:50:19 +0200

    - add more modules to Module data - deletion of ContentView() - creation of ModuleDetailView

---
+++ FireLearn/ModuleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ModuleDetailView.swift
+//  FireLearn
+//
+//  Created by Apprenant 167 on 05/06/2024.
+//
+
+import SwiftUI
+
+struct ModuleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModuleDetailView()
+}
---
+++ FireLearn/ModuleListView.swift
@@ -0,0 +1,18 @@
+//
+//  ModuleListView.swift
+//  FireLearn
+//
+//  Created by Apprenant 167 on 05/06/2024.
+//
+
+import SwiftUI
+
+struct ModuleListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModuleListView()
+}
